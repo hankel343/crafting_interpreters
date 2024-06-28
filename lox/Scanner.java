16fed1c61e6961bad682109c08e26ef10314230e
hhaldin <haldin@iastate.edu>
2024-06-28 10:16:31 -0500

    add method scanTokens()

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -17,4 +17,15 @@ class Scanner {
     Scanner(String source) {
         this.source = source;
     }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // at the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
 }
