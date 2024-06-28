a48066abf61145910661df75d6873f53efaf1248
hhaldin <haldin@iastate.edu>
2024-06-28 10:15:59 -0500

    add fields to Scanner.java for parsing

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -10,6 +10,9 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayLlist<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
