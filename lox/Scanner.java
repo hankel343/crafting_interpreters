48b4d1e2142f1ccd7ea2c89d346f88cadeae17dc
hhaldin <haldin@iastate.edu>
2024-06-28 10:32:06 -0500

    add error handling in scanTokens()

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -42,6 +42,10 @@ class Scanner {
             case '+':   addToken(PLUS);         break;
             case ';':   addToken(SEMICOLON);    break;
             case '*':   addToken(START);        break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
     }
 
