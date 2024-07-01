106b7a1ecbf3bb13340faf253a44ca7aeb10aaae
hhaldin <haldin@iastate.edu>
2024-07-01 15:56:31 -0500

    add unary visitor expr method

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -8,6 +8,19 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                    return -(double)right;
+        }
+
+        // Unreachable
+        return null;
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
