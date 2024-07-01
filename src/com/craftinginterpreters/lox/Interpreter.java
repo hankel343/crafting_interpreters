563549fe131ab706c77cc25a2d7b68371aa4881f
hhaldin <haldin@iastate.edu>
2024-07-01 15:38:54 -0500

    add visitor pattern for grouping exprs

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,8 +1,15 @@
 package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
 }
