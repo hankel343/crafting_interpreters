d839e7e1c475a460bd58dc7a44e1e13de504b163
hankel.haldin <hankel343@gmail.com>
2024-07-02 21:49:49 -0500

    comment wip class

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,31 +1,31 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
-
-    @Override
-    public Object visitLiteralExpr(Expr.Literal expr) {
-        return expr.value;
-    }
-
-    @Override
-    public Object visitUnaryExpr(Expr.Unary expr) {
-        Object right = evaluate(expr.right);
-
-        switch (expr.operator.type) {
-            case MINUS:
-                    return -(double)right;
-        }
-
-        // Unreachable
-        return null;
-    }
-
-    @Override
-    public Object visitGroupingExpr(Expr.Grouping expr) {
-        return evaluate(expr.expression);
-    }
-
-    private Object evaluate(Expr expr) {
-        return expr.accept(this);
-    }
-}
+// class Interpreter implements Expr.Visitor<Object> {
+// 
+//     @Override
+//     public Object visitLiteralExpr(Expr.Literal expr) {
+//         return expr.value;
+//     }
+// 
+//     @Override
+//     public Object visitUnaryExpr(Expr.Unary expr) {
+//         Object right = evaluate(expr.right);
+// 
+//         switch (expr.operator.type) {
+//             case MINUS:
+//                     return -(double)right;
+//         }
+// 
+//         // Unreachable
+//         return null;
+//     }
+// 
+//     @Override
+//     public Object visitGroupingExpr(Expr.Grouping expr) {
+//         return evaluate(expr.expression);
+//     }
+// 
+//     private Object evaluate(Expr expr) {
+//         return expr.accept(this);
+//     }
+// }
