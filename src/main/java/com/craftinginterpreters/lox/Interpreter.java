a49ecd319dbeddd6c5fb9b5b0e435a93fae2cba4
hhaldin <hankel343@gmail.com>
2024-07-08 21:02:00 -0500

    implement operators for binary expressions

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -37,4 +37,35 @@ class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch(expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;
+
+            case SLASH:
+                return (double)left / (double)right;
+
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Unreachable
+        return null;
+    }
 }
