1603314b6df65a025220a673821598abf6150310
hhaldin <hankel343@gmail.com>
2024-07-08 21:22:23 -0500

    implement runtime error checking for all operations

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -29,6 +29,12 @@ class Interpreter implements Expr.Visitor<Object> {
         throw new RuntimeError(operator, "Operand must be a number.");
     }
 
+    private void checkNumberOperand(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double)  return;
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
     private boolean isTruthy(Object object) {
         if (object == null) return false;
         if (object instanceof Boolean) return (boolean)object;
@@ -58,15 +64,19 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch(expr.operator.type) {
             case GREATER:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left > (double)right;
             
             case GREATER_EQUAL:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left >= (double)right;
             
             case LESS:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left < (double)right;
             
             case LESS_EQUAL:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left <= (double)right;
 
             case BANG_EQUAL: return !isEqual(left, right);
@@ -84,12 +94,14 @@ class Interpreter implements Expr.Visitor<Object> {
                     return (String)left + (String)right;
                 }
 
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
 
             case SLASH:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left / (double)right;
 
             case STAR:
+                checkNumberOperand(expr.operator, left, right);
                 return (double)left * (double)right;
         }
 
