a9474e06cb91857b0fc17f053c4cf44691d74876
hhaldin <hankel343@gmail.com>
2024-07-08 21:31:50 -0500

    add runtime error handling to Lox.java

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,7 +28,8 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         // indicate an error in the exit code.
-        if (hadError)   System.exit(65);
+        if (hadError)           System.exit(65);
+        if (hadRuntimeError)    System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -71,5 +73,10 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
