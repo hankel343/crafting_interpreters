b9ef0a826b5d9137fc5db7a446e12c904ba97d72
hhaldin <hankel343@gmail.com>
2024-07-08 20:57:15 -0500

    add truthy / falsey values

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,31 +1,40 @@
 package com.craftinginterpreters.lox;
 
-// class Interpreter implements Expr.Visitor<Object> {
-// 
-//     @Override
-//     public Object visitLiteralExpr(Expr.Literal expr) {
-//         return expr.value;
-//     }
-// 
-//     @Override
-//     public Object visitUnaryExpr(Expr.Unary expr) {
-//         Object right = evaluate(expr.right);
-// 
-//         switch (expr.operator.type) {
-//             case MINUS:
-//                     return -(double)right;
-//         }
-// 
-//         // Unreachable
-//         return null;
-//     }
-// 
-//     @Override
-//     public Object visitGroupingExpr(Expr.Grouping expr) {
-//         return evaluate(expr.expression);
-//     }
-// 
-//     private Object evaluate(Expr expr) {
-//         return expr.accept(this);
-//     }
-// }
+class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+
+            case MINUS:
+                    return -(double)right;
+        }
+
+        // Unreachable
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+}
