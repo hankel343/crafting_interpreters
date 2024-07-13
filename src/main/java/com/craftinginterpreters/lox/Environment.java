e4f105f2d015dfb7f2b3283dc90630f3615e3ef0
hhaldin <hankel343@gmail.com>
2024-07-13 14:51:19 -0500

    11.4.1

--- src/main/java/com/craftinginterpreters/lox/Environment.java
+++ src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -38,7 +38,21 @@ class Environment {
         
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
+
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
     }
 }
