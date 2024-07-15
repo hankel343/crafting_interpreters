77d36b8169434e15e1324b287f65fce36ab75f9e
hankel343 <haldin@iastate.edu>
2024-07-15 14:05:21 -0500

    update directory structure

--- scripts/utility/update_ast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $HOME/git/crafting_interpreters/src/main/java/
-echo "Compiling GenerateAst.java..."
-javac com/craftinginterpreters/tool/GenerateAst.java
-echo "Running GenerateAst.class..."
-java com.craftinginterpreters.tool.GenerateAst com/craftinginterpreters/lox/
-rm com/craftinginterpreters/tool/GenerateAst.class
-echo "Done."
