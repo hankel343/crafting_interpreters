04e57a9f6532a441b3716b3755d9d45b011104fe
hankel343 <haldin@iastate.edu>
2024-07-30 14:03:59 -0500

    28.2

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -73,6 +73,18 @@ static void blackenObject(Obj* object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
+        case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            markTable(&klass->methods);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*)object;
             markObject((Obj*)closure->function);
@@ -108,9 +120,13 @@ static void freeObject(Obj* object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD:
+            FREE(ObjBoundMethod, object);
+            break;
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
-            markObject((Obj*)klass->name);
+            freeTable(&klass->methods);
+            FREE(ObjClass, object);
             break;
         }
         case OBJ_CLOSURE: {
